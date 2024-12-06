f1364906ad7537611bef853f375765da45d5aea4
JD84395 <157139248+JD84395@users.noreply.github.com>
2024-12-06 12:18:11 -0500

    Update urls.py

--- Back end/Web App/urls.py
+++ Back end/Web App/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import get_users, create_user, update_user
+from .views import get_users, create_user, update_user, main, login, signup, delete_user
 
 
 urlpatterns = [
     path('users/', get_users, name='get_users'),
     path('users/create', create_user, name='create_user'),
-    path('users/<int:pk>', update_user, name='update_user')
+    path('users/<int:pk>', update_user, name='update_user'),
+    path('', main, name='main'),
+    path('login/', login, name='login'),
+    path('signup/', signup, name='signup'),
+    path('users/delete', delete_user, name='delete_user')
 ]
